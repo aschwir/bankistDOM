29ed73d80ed9606162361081433b67d9d13e7043
andrew schwirzinski <aschwirzinski@gmail.com>
2022-08-03 12:49:56 -0400

    random color on nav

--- script.js
+++ script.js
@@ -123,3 +123,36 @@ btnScrollTo.addEventListener("click", function (e) {
 // logo.classList.remove();
 // logo.classList.toggle();
 // logo.classList.contains();
+
+// const alertH1 = function (e) {
+//   alert("addEventListener");
+// };
+
+// const h1 = document.querySelector("h1");
+// h1.addEventListener("mouseenter", alertH1);
+
+// setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+
+//rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+console.log(randomColor(0, 255));
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+
+  //stop event propagation(bad practice)
+  e.stopPropagation();
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector(".nav").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+});
